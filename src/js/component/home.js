c2c6b7e3d4f3b36a6c0be904176d589ab473fc24
GuillermoCairo26 <guillermocairo14@gmail.com>
2021-06-22 06:13:59 +0000

    finalizado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,18 +7,125 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
+			<h1>Full-Stack Final Project.</h1>
+			<h5>Número de grupo: 2.</h5>
+			<h5>
+				Integrantes del grupo: Guillermo Cairo, Bernardo Piuma, Lucia
+				Yebenes, Andrés Campopiano.
+			</h5>
+			<h5>Nombre de aplicación: Wine and Senses.</h5>
+			<h5>Idea del proyecto:</h5>
+			<h5>
+				Crear una aplicación que nos permita visualizar una tienda
+				online de vinos con registro de usuario y demás funcionalidades,
+				donde proveedores de vinos puedan publicar sus productos y que
+				cuente con una funcionalidad de “cata virtual” con la cual un
+				usuario pueda agendar un evento de cata, pagar un precio, y
+				recibir los productos para degustar en su casa con la
+				posibilidad de unirse a una reunión virtual para compartir la
+				degustación con demás interesados y un enólogo que la guíe.
+			</h5>
+
+			<a
+				href="https://github.com/bpiuma/proyecto-final-frontend.git"
+				target="_blank"
+				rel="noreferrer">
+				Link repositorio front-end:
+				https://github.com/bpiuma/proyecto-final-frontend.git
 			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<br></br>
+			<br></br>
+			<a
+				href="https://github.com/bpiuma/proyecto-final-backend.git"
+				target="_blank"
+				rel="noreferrer">
+				Link repositorio front-end:
+				https://github.com/bpiuma/proyecto-final-backend.git
+			</a>
+			<br></br>
+			<br></br>
+			<a
+				href="https://www.programmableweb.com/api/global-wine-score-rest-api"
+				target="_blank"
+				rel="noreferrer">
+				Link API utilizada:
+				https://www.programmableweb.com/api/global-wine-score-rest-api
+			</a>
+			<br></br>
+			<br></br>
+			<h5>Aplicación que usamos para creación de wireframes: Figma</h5>
+			<a
+				href="https://www.figma.com/file/O8YOl1dhatquOxxdE8Z4gx/Untitled?node-id=0%3A1"
+				target="_blank"
+				rel="noreferrer">
+				Link Figma:
+				https://www.figma.com/file/O8YOl1dhatquOxxdE8Z4gx/Untitled?node-id=0%3A1
+			</a>
+			<br></br>
+			<br></br>
+			<h5>
+				Aplicación utilizada para la organización del proyecto: Trello.
+			</h5>
+			<a
+				href="https://trello.com/b/esrIE2QP/proyecto-final-wine-senses"
+				target="_blank"
+				rel="noreferrer">
+				Link Trello:
+				https://trello.com/b/esrIE2QP/proyecto-final-wine-senses
+			</a>
+			<br></br>
+			<br></br>
+			<h5>Historias de usuario</h5>
+			<h6>Como usuario quiero poder registrarme.</h6>
+			<h6>Como usuario quiero poder ingresar a mi cuenta.</h6>
+			<h6>Como usuario quiero poder salir de mi cuenta.</h6>
+			<h6>Como usuario quiero actualizar mis datos personales.</h6>
+			<h6>Como usuario quiero recuperar mi contraseña.</h6>
+			<h6>Como usuario quiero poder ver eventos de cata virtual.</h6>
+			<h6>
+				Como usuario quiero poder agendarme a eventos de cata virtual.
+			</h6>
+			<h6>Como usuario quiero ver el catálogo de vinos.</h6>
+			<h6>
+				Como usuario quiero poder agregar vinos del catálogo a mis
+				favoritos.
+			</h6>
+			<h6>
+				Como usuario quiero obtener mi carrito de compra y el importe
+				total.
+			</h6>
+			<h6>
+				Como usuario quiero poder eliminar/agregar un producto de mi
+				carrito de compras.
+			</h6>
+			<h6>
+				Como usuario quiero poder contactarme para publicar mi producto.
+			</h6>
+			<br></br>
+			<h5>Publicidad para el producto:</h5>
+			<a
+				href="https://drive.google.com/file/d/1ZEHUc3A81-KsN2WE39N_PfLofpQ3hmRX/view?usp=sharing"
+				target="_blank"
+				rel="noreferrer">
+				Link Video promocional y demo:
+				https://drive.google.com/file/d/1ZEHUc3A81-KsN2WE39N_PfLofpQ3hmRX/view?usp=sharing
+			</a>
+			<br></br>
+			<br></br>
+			<h5>Google Jamboard:</h5>
+			<a
+				href="https://jamboard.google.com/d/1AI3hmdLKx6YhVw7Sz-vfcdR3_fgtISGsJ0_0vHB_Pzo/viewer?f=0"
+				target="_blank"
+				rel="noreferrer">
+				Link Google Jamboard:
+				https://jamboard.google.com/d/1AI3hmdLKx6YhVw7Sz-vfcdR3_fgtISGsJ0_0vHB_Pzo/viewer?f=0
+			</a>
+			<br></br>
+			<br></br>
+			<h5>Agradecimiento especial a : 4Geeks Academy, UTEC, BID</h5>
+			<br></br>
+			<br></br>
+			<br></br>
 		</div>
 	);
 }
